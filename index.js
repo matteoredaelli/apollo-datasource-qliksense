84b759b79b80e33bcbb5a1b35618b751be12caf9
Matteo Redaelli <matteo.redaelli@gmail.com>
2021-03-27 22:44:09 +0100

    added filters and count

--- index.js
+++ index.js
@@ -19,41 +19,50 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-const https = require('https');
-const fs = require('fs');
-const { RESTDataSource } = require('apollo-datasource-rest');
-const getXrfKey  = () => {
-    return "1234567890123456";
-}
+const https = require("https");
+const fs = require("fs");
+const { RESTDataSource } = require("apollo-datasource-rest");
+const getXrfKey = () => {
+  return "1234567890123456";
+};
 
 class QliksenseDataSource extends RESTDataSource {
-    constructor(baseurl, cert_file, key_file) {
-	super();
-	this.baseURL = baseurl;
-	this.sslConfiguredAgent = new https.Agent({
-	    rejectUnauthorized: false, // (NOTE: this will disable client verification)
-	    cert: fs.readFileSync(cert_file),
-	    key: fs.readFileSync(key_file),
-	    //passphrase: "YYY",
-	    keepAlive: false, // switch to true if you're making a lot of calls from this client
-	});
-    }
+  constructor(baseurl, cert_file, key_file) {
+    super();
+    this.baseURL = baseurl;
+    this.sslConfiguredAgent = new https.Agent({
+      rejectUnauthorized: false, // (NOTE: this will disable client verification)
+      cert: fs.readFileSync(cert_file),
+      key: fs.readFileSync(key_file),
+      //passphrase: "YYY",
+      keepAlive: false // switch to true if you're making a lot of calls from this client
+    });
+  }
 
-    willSendRequest(request) {
-	request.params.set('xrfkey', getXrfKey());
-	request.headers.set('Accept', 'application/json');
-	request.headers.set('Content-Type', 'application/json');
-	request.headers.set('x-qlik-xrfkey', getXrfKey());
-	request.headers.set('X-Qlik-User', 'UserDirectory= Internal;UserId=sa_repository');
-	request.agent = this.sslConfiguredAgent;
-    }
+  willSendRequest(request) {
+    request.params.set("xrfkey", getXrfKey());
+    request.headers.set("Accept", "application/json");
+    request.headers.set("Content-Type", "application/json");
+    request.headers.set("x-qlik-xrfkey", getXrfKey());
+    request.headers.set(
+      "X-Qlik-User",
+      "UserDirectory= Internal;UserId=sa_repository"
+    );
+    request.agent = this.sslConfiguredAgent;
+  }
 
-    async getEntity(entity) {
-	const self = this;
-	const resp = await this.get(`/qrs/${entity}/full`); // , null, {agent: self.sslConfiguredAgent}); // (`/qrs/${entity}/full`,
-	console.log(resp)
-	return {text:  JSON.stringify(resp) };
-    }
-};
+  async getEntity(entity, count = false, filter = null) {
+    const full_or_count = count ? "count" : "full";
+    let url = `/qrs/${entity}/${full_or_count}`;
+    //if (filter) {
+    //  url = url + `?filter=${filter}`;
+    //}
+    const options = filter ? { params: { filter: filter } } : null;
+    console.log(options);
+    const resp = await this.get(url, null, options); // {agent: self.sslConfiguredAgent});
+    console.log(resp);
+    return resp;
+  }
+}
 
 module.exports = { QliksenseDataSource };
